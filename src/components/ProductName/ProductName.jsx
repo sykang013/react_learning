1f251f941151b295dc794df94677dad2dc9af6ee
sykang013 <113448786+sykang013@users.noreply.github.com>
2023-02-28 00:12:48 +0900

    1 binaries uploaded

--- src/components/ProductName/ProductName.jsx
+++ src/components/ProductName/ProductName.jsx
@@ -8,9 +8,13 @@ export default function ProductName() {
     <div>
       {/* 퍼블릭폴더것은 경로설정없이 자동으로 가져와지는데, src/assets에서 어떻게 가져오는지 모르겠다. 계속 실패. */}
       <img className={classes.productImage}
-        src="productImg_Rapigen_1x.png"
+        src="../../../public/productImg_Rapigen_1x.png"
         alt="래피젠 코로나 자가 검사 키트 상품 이미지"
       />
+      {/* <img className={classes.productImage}
+      src="productImg_Rapigen_1x.png"
+      alt="래피젠 코로나 자가 검사 키트 상품 이미지"
+    /> */}
       <h1 className={classes.productInfo}>래피젠 코로나 자가 검사 키트<br></br> 2개입X1박스</h1>
       {/* <h1 className={classes.productInfo}>{name}</h1> */}
     </div>
