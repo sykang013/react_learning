4eb664bf8dfb28173c3986c13ee163351b6dbf67
sykang013 <sy.kang013@gmail.com>
2023-03-05 19:41:13 +0900

    close #6 feat. 카운터 버튼 +, - 작동 구현

--- src/components/PurchaseCount/PurchaseCount.jsx
+++ src/components/PurchaseCount/PurchaseCount.jsx
@@ -1,15 +1,30 @@
-import React from 'react';
+//리액트에서 상태변수 추가
+import React, {useState} from 'react';
 import classes from './PurchaseCount.module.css';
 
 export default function PurchaseCount() {
+  //리액트 훅인 useState의 문법이다. const [sate, setState] = useState(initialState) const도 관례. 
+  const [number, setNumber] = useState(1); 
+  const plusButton = () => {
+    setNumber(number + 1);
+  }
+  //number가 0이면, return으로 문을 빠져나온다.
+  const minusButton = () =>{
+    if(number === 0){
+      return;
+    } else(
+      setNumber(number - 1)
+    )
+  }
+
   return (
-    //section에 막혀서 클릭 이벤트 안되는 상황.이벤트 위임??
-    <section type="button" className={classes.purchaseCount}>
-      <button className={classes.button}>-</button>
-      <span className={classes.countState}>1</span>
-      <button type="button" className={classes.button}>
+    <section className={classes.purchaseCount}>
+      <button onClick={minusButton} type="button" className={classes.minusButton}>-</button>
+      <span className={classes.countState}>{number}</span>
+      <button onClick={plusButton} type="button" className={classes.plusButton}>
         +
       </button>
     </section>
   );
 }
+
